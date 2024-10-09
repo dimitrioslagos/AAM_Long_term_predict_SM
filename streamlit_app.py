46a4cd903650d3c1c779f20fc010ea454a92df75
Dimitris <dimitrios_lagos@hotmail.com>
2024-10-09 12:41:22 +0300

    update code for map plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,4 +136,4 @@ with tabs[3]:
         plot_network_with_lf_res(topology, probability_SM[probability_SM['Failure Probability'] >= 70])
         with open("network_map.html", 'r', encoding='utf-8') as file:
             html_content = file.read()
-        st.components.v1.html()(html_content, width=1000, height=400, scrolling=True)
+        st.components.v1.html(html_content, width=1000, height=400, scrolling=True)
